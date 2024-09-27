11253a362e3ae97ff080c89bd0c8b57533356b3c
sadeeq <sadeequllahkhan837@gmail.com>
2024-09-27 12:02:36 +0500

    initial commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const { ensureAthenticated } = require('../middlewares/auth.middleware');
-const authController = require('../controllers/auth.controller');
+// const authController = require('../controllers/auth.controller');
 
 module.exports = router;
